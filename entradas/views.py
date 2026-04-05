da50833c98d591ee99e6474f4e33015a9b7a6803
Luisf66 <Luisf311220@gmail.com>
2026-04-05 14:11:08 -0300

    style: Alterações visuais no CRUD categoria #5

--- entradas/views.py
+++ entradas/views.py
@@ -34,36 +34,31 @@ class Categorias_EntradasDeleteView(DeleteView):
     template_name = 'categorias_entrada/categorias_entradas_delete.html'
     success_url = reverse_lazy('entradas:categorias-entradas-list')
 
-class EntradasCreateView(CreateView):
-    model = Entradas
-    form_class = EntradasForm
-    template_name = 'entradas_create.html'
-    success_url = reverse_lazy('entradas:entradas-list')
-
-'''    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['categorias_entradas'] = Categorias_Entradas.objects.all()
-        return context
-'''
-class EntradasListView(ListView):
-    model = Entradas
-    template_name = 'entradas_list.html'
-    context_object_name = 'entradas'
-
-class EntradasUpdateView(UpdateView):
-    model = Entradas
-    form_class = EntradasForm
-    template_name = 'entradas_update.html'
-    success_url = reverse_lazy('entradas:entradas-list')
-
-class EntradasDeleteView(DeleteView):
-    model = Entradas
-    template_name = 'entradas_delete.html'
-    success_url = reverse_lazy('entradas:entradas-list')
-
-    def delete(self, request, *args, **kwargs):
+    def post(self, request, *args, **kwargs):
         try:
             return super().delete(request, *args, **kwargs)
         except ProtectedError as e:
             messages.error(request, f"Erro ao excluir: {e.protected_objects}")
             return redirect(self.success_url)
+
+class EntradasCreateView(CreateView):
+    model = Entradas
+    form_class = EntradasForm
+    template_name = 'entradas/entradas_create.html'
+    success_url = reverse_lazy('entradas:entradas-list')
+
+class EntradasListView(ListView):
+    model = Entradas
+    template_name = 'entradas/entradas_list.html'
+    context_object_name = 'entradas'
+
+class EntradasUpdateView(UpdateView):
+    model = Entradas
+    form_class = EntradasForm
+    template_name = 'entradas/entradas_update.html'
+    success_url = reverse_lazy('entradas:entradas-list')
+
+class EntradasDeleteView(DeleteView):
+    model = Entradas
+    template_name = 'entradas/entradas_delete.html'
+    success_url = reverse_lazy('entradas:entradas-list')
