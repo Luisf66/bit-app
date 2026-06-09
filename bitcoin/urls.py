3f7d494509d3a559068e1e885ef62a66254ab78c
Luisf66 <Luisf311220@gmail.com>
2026-06-09 20:44:32 -0300

    feat: Adição de botão para analise com IA #29

--- bitcoin/urls.py
+++ bitcoin/urls.py
@@ -11,6 +11,7 @@ urlpatterns = [
     path('upload/', views.bitcoin_upload_view, name='bitcoin-upload'),
     path('list/', views.TransacaoListView.as_view(), name='bitcoin-list'),
     path('dashboard/', views.dashboard_view, name='bitcoin-dashboard'),
+    path('dashboard/analise/', views.gerar_analise_view, name='gerar-analise'),
 ]
 
 #urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
