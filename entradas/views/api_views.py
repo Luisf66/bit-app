88da580bde7d55c32049b79780abeae03c927b28
Luisf66 <Luisf311220@gmail.com>
2026-06-13 21:03:31 -0300

    feat: API views #44

---
+++ entradas/views/api_views.py
@@ -0,0 +1,29 @@
+from rest_framework import viewsets, permissions
+from entradas.models import Entradas, Categorias_Entradas
+from entradas.serializer.entrada_serializer import EntradaSerializer, CategoriaEntradaSerializer
+
+
+class CategoriaEntradaViewSet(viewsets.ModelViewSet):
+    serializer_class = CategoriaEntradaSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        return Categorias_Entradas.objects.filter(usuario=self.request.user)
+
+    def perform_create(self, serializer):
+        serializer.save(usuario=self.request.user)
+
+
+class EntradaViewSet(viewsets.ModelViewSet):
+    serializer_class = EntradaSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        return Entradas.objects.filter(usuario=self.request.user).order_by('-data')
+
+    def perform_create(self, serializer):
+        serializer.save(usuario=self.request.user)
+
+    def get_serializer_context(self):
+        context = super().get_serializer_context()
+        return context
